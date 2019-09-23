5fb06b94cbae90b7b0b3d47590e683e7e7fab29d
summaity <summaity@SUMMAITY-LAP.oradev.oraclecorp.com>
2019-09-23 22:08:39 +0530

    changed

--- Learn/src/Topics/ComaratorC.java
+++ Learn/src/Topics/ComaratorC.java
@@ -40,25 +40,5 @@ public class ComaratorC{
 	}
 
 
-	public static Comparator<ComaratorC> ageCompare= new Comparator<ComaratorC>() {
-	
-
-	@Override
-	public int compare(ComaratorC arg0, ComaratorC arg1) {
-		// TODO Auto-generated method stub
-		int a=arg0.getAge();
-		int b=arg1.getAge();
-		return a-b;
-	}};
-	
-	public static Comparator<ComaratorC> nameCompare= new Comparator<ComaratorC>() {
-		
-
-		@Override
-		public int compare(ComaratorC arg0, ComaratorC arg1) {
-			// TODO Auto-generated method stub
-			//ascending order
-			return arg0.getName().compareTo(arg1.getName());
-		}};
 	
 }
