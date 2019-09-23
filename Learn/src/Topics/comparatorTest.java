c6dbc93671d139985434a12335bea9e0ca7a07b0
summaity <summaity@SUMMAITY-LAP.oradev.oraclecorp.com>
2019-09-23 22:08:23 +0530

    change

--- Learn/src/Topics/comparatorTest.java
+++ Learn/src/Topics/comparatorTest.java
@@ -7,20 +7,22 @@ public class comparatorTest {
 	public static void main(String[] args) {
 		ComaratorC ac0= new ComaratorC(29, "mrnav");
 		ComaratorC ac1= new ComaratorC(97, "zarun");
+		ComaratorC ac3= new ComaratorC(97, "arun");
 		ComaratorC ac2= new ComaratorC(20,"aumita");
 		
 		ArrayList<ComaratorC> li=new ArrayList<ComaratorC>();
 		li.add(ac0);
 		li.add(ac1);
 		li.add(ac2);
-		
-		Collections.sort(li, ComaratorC.ageCompare);
+		li.add(ac3);
+		Collections.sort(li, ComparatorC.ageCompare);
 		
 		for(ComaratorC a:li) {
 			System.out.println(a);
+			
 		}
-		
-		Collections.sort(li, ComaratorC.nameCompare);
+		System.out.println("---------------------------------------");
+		Collections.sort(li, ComparatorC.nameCompare);
 		
 		for(ComaratorC a:li) {
 			System.out.println(a);
